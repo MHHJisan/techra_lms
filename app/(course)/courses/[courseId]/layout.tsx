0ccf0fbfcecbbc61c86628802301b1b2d0ab3d43
jisan <mhhaque4@gmail.com>
2024-11-08 14:41:25 +0600

    adding mobile sidebar in navbar

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { CourseSidebar } from "./_components/course-sidebar";
+import { CourseNavbar } from "./_components/course-navbar";
 
 const CourseLayout = async ({
   children,
@@ -48,10 +49,13 @@ const CourseLayout = async ({
 
   return (
     <div className="h-full flex">
+      <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
+        <CourseNavbar course={course} progressCount={progressCount} />
+      </div>
       <div className="hidden md:flex w-80 flex-col fixed inset-y-0 left-0 z-50">
         <CourseSidebar course={course} progressCount={progressCount} />
       </div>
-      <main className="flex-1 md:pl-80 h-full">{children}</main>
+      <main className="flex-1s md:pl-80 pt-[80px] h-full">{children}</main>
     </div>
   );
 };
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -13,7 +13,7 @@ export const NavbarRoutes = () => {
   const router = useRouter();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
-  const isPlayerPage = pathname?.includes("/chapter");
+  const isPlayerPage = pathname?.includes("/courses");
   const isSearchPage = pathname === "/search";
 
   return (
