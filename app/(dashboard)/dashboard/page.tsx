65e33a293da7beca98bed7cdd40493ac80407ecb
jisan <mhhaque4@gmail.com>
2024-11-28 11:51:44 +0600

    added completed course in student dashboard

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,20 @@
+import { getDashboardCourses } from "@/actions/get-dashboard-courses";
+import { CoursesList } from "@/components/courses-list";
 import { auth, clerkClient } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { InfoCard } from "./_components/info-card";
+import { CheckCircle, Clock } from "lucide-react";
 
 export default async function DashboardPage() {
   const { userId } = auth();
 
   if (!userId) {
-    return <p>Redirecting...</p>;
+    return redirect("/");
   }
+
+  const { completedCourses, coursesInProgress } = await getDashboardCourses(
+    userId
+  );
 
   // Fetch user details from Clerk
   const user = await clerkClient.users.getUser(userId);
@@ -22,6 +31,22 @@ export default async function DashboardPage() {
         Welcome to your dashboard,{" "}
         <span className="text-xl font-bold text-green-500">{username}!</span>
       </h1>
+      <div className="p-6 space-y-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <InfoCard
+            icon={Clock}
+            label="In Progress"
+            numberOfItems={coursesInProgress.length}
+          />
+          <InfoCard
+            icon={CheckCircle}
+            label="Completed"
+            numberOfItems={completedCourses.length}
+            variant="success"
+          />
+        </div>
+      </div>
+      <CoursesList items={[...coursesInProgress, ...completedCourses]} />
     </div>
   );
 }
